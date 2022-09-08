fc2c5d7e909c6ca74d253d65b053b453a532d53b
Masha Sechina <mashka_3@mail.ru>
2022-09-08 13:38:02 +0300

    homework-2.1

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int percent;
+        int price = 1000;
+        int rublesForOneMileForOneMile = 20;
+
+        percent = price / rublesForOneMileForOneMile;
+
+        System.out.println("Начислено мили за купленный билет: "+ percent);
+
+
+    }
+}
